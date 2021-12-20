a27700b208f96e33ae6dfc85cd9900bcafc937db
Ali Movahedi <ali_movahedi@aol.com>
2021-12-20 15:54:00 +0330

    add test for chain

--- chain_test.go
+++ chain_test.go
@@ -7,7 +7,7 @@ import (
 	"time"
 )
 
-func TestNewChain(t *testing.T) {
+func TestFindFreeNode(t *testing.T) {
 	chainNode1 := NewChainNode(ChainNodeData{
 		name:           "Node 1",
 		url:            "http://example.com",
@@ -28,5 +28,75 @@ func TestNewChain(t *testing.T) {
 
 	foundNode := createdChain.getFreeNode()
 
-	assert.Equal(t, chainNode1.id, foundNode.id)
+	assert.NotNil(t, foundNode, "should find node")
+	if foundNode != nil {
+		assert.Equal(t, chainNode1.id, foundNode.id, "should found node id be correct")
+	}
+}
+
+func TestNotFindNode(t *testing.T) {
+	chainNode1 := NewChainNode(ChainNodeData{
+		name:           "Node 1",
+		url:            "http://example.com",
+		limit:          NewChainNodeLimit(10, 2*time.Second),
+		requestTimeout: 1 * time.Second,
+		priority:       1,
+		middleware: func(request *http.Request) *http.Request {
+			return request
+		},
+	})
+
+	createdChain := NewChain(
+		[]*ChainNode{
+			chainNode1,
+		},
+		NewCheckTick(500*time.Millisecond, 1*time.Second),
+	)
+
+	chainNode1.hits = 10
+
+	foundNode := createdChain.getFreeNode()
+
+	assert.Nil(t, foundNode, "should not find node")
+}
+
+func TestLoadBalance(t *testing.T) {
+	chainNode1 := NewChainNode(ChainNodeData{
+		name:           "Node 1",
+		url:            "http://example.com",
+		limit:          NewChainNodeLimit(10, 2*time.Second),
+		requestTimeout: 1 * time.Second,
+		priority:       1,
+		middleware: func(request *http.Request) *http.Request {
+			return request
+		},
+	})
+
+	chainNode2 := NewChainNode(ChainNodeData{
+		name:           "Node 2",
+		url:            "http://example.com",
+		limit:          NewChainNodeLimit(10, 2*time.Second),
+		requestTimeout: 1 * time.Second,
+		priority:       1,
+		middleware: func(request *http.Request) *http.Request {
+			return request
+		},
+	})
+
+	createdChain := NewChain(
+		[]*ChainNode{
+			chainNode1,
+			chainNode2,
+		},
+		NewCheckTick(500*time.Millisecond, 1*time.Second),
+	)
+
+	chainNode1.hits = 1
+	foundNode := createdChain.getFreeNode()
+	assert.Equal(t, chainNode2.id, foundNode.id, "should route to node 2")
+
+	chainNode2.hits = 3
+	chainNode1.hits = 2
+	foundNode = createdChain.getFreeNode()
+	assert.Equal(t, chainNode1.id, foundNode.id, "should route to node 1")
 }
